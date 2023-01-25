20d822d4859e104e453e2358aafa3670154f5d4b
Renato Almeida de Oliveira <renato.almeida.oliveira@gmail.com>
2023-01-25 21:52:43 +0000

    Change navigation to Menu

--- nb_risk/navigation.py
+++ nb_risk/navigation.py
@@ -1,20 +1,39 @@
-from extras.plugins import PluginMenuItem
+from extras.plugins import PluginMenu, PluginMenuItem
 
-menu_items = (
-    PluginMenuItem(
-        link="plugins:nb_risk:threatsource_list",
-        link_text="Threat Sources",
-    ),
-    PluginMenuItem(
-        link="plugins:nb_risk:threatevent_list",
-        link_text="Threat Events",
-    ),
-    PluginMenuItem(
-        link="plugins:nb_risk:vulnerability_list",
-        link_text="Vulnerabilities",
-    ),
-    PluginMenuItem(
-        link="plugins:nb_risk:risk_list",
-        link_text="Risks",
+menu = PluginMenu(
+    label="Risk Assessment",
+    icon_class="mdi mdi-spider",
+    groups=(
+        (
+            "Threat",
+            (
+                PluginMenuItem(
+                    link="plugins:nb_risk:threatsource_list",
+                    link_text="Threat Sources",
+                ),
+                PluginMenuItem(
+                    link="plugins:nb_risk:threatevent_list",
+                    link_text="Threat Events",
+                ),
+            ),
+        ),
+        (
+            "Vulnerability",
+            (
+                PluginMenuItem(
+                    link="plugins:nb_risk:vulnerability_list",
+                    link_text="Vulnerabilities",
+                ),
+            ),
+        ),
+        (
+            "Risk",
+            (
+                PluginMenuItem(
+                    link="plugins:nb_risk:risk_list",
+                    link_text="Risks",
+                ),
+            ),
+        ),
     ),
 )
