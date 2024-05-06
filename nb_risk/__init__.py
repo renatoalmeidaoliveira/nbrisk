74230db9417d2705659d29e799cdc811a221eb88
Renato Almeida de Oliveira <renato.almeida.oliveira@gmail.com>
2024-05-06 23:30:04 +0000

    Version 4.0.0 support

--- nb_risk/__init__.py
+++ nb_risk/__init__.py
@@ -11,7 +11,7 @@ class NbriskConfig(PluginConfig):
     version = __version__
     author = "Renato Almdida Oliveira"
     author_email = "renato.almeida.oliveira@gmail.com"
-#    min_version = "4.0.0"
+    min_version = "4.0.0"
     max_version = "4.0.99"
     required_settings = []
     default_settings = {
